6fb61b6ac81ced33eb2de712a62af306e821a4aa
mdetloff1 <martin.detloff@gmail.com>
2026-04-18 12:27:46 -0600

    Initial Directory commit

---
+++ MainApp.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application
+{   
+    public static void main(String[] args) 
+    {
+        launch(args);
+        System.out.println("Finished");       
+    }
+
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        primaryStage.setScene(new Scene(new GridPane(), 400, 400));
+        primaryStage.show();
+    }
+}
